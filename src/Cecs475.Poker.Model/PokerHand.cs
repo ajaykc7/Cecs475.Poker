55779527a264bd521c77f3906d50e575236585d3
ajaykc7 <azay.kc13@gmail.com>
2018-01-31 22:45:37 -0800

    First Commit

---
+++ src/Cecs475.Poker.Model/PokerHand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cecs475.Poker.Cards
+{
+    class PokerHand
+    {
+
+    }
+}
---
+++ test/Cecs475.Poker.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cecs475.Poker.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
